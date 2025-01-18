e3c84471436373dbd8275c6217028a14803714bb
Lucas Freitas Cordeiro <93345102+Bariand@users.noreply.github.com>
2025-01-18 19:30:22 -0300

    Input System pronto
    Input system finalizado para 2 players

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,47 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    //"InputSystem_Actions" é o nome da "Input Action Asset", ou seja, da lista de input que fizemos no início
-    private InputSystem_Actions playerControls;
     private Rigidbody2D rb;
     [SerializeField] private float moveSpeed;
+    [SerializeField] private bool isPlayerOne;  // Define se este pad é o Player 1
 
     void Start()
     {
-        playerControls = new InputSystem_Actions();
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void OnMove(InputValue inputValue) 
-    {   
-        rb.linearVelocity = inputValue.Get<Vector2>() * moveSpeed;
-        print("OnMove foi ativado");
+    void Update()
+    {
+        float moveInput = 0;
+
+        // Verificar se este pad é o Player 1 ou Player 2
+        if (isPlayerOne)
+        {
+            // Controles para o Player 1 (W e S)
+            if (Input.GetKey(KeyCode.W))
+            {
+                moveInput = 1;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                moveInput = -1;
+            }
+        }
+        else
+        {
+            // Controles para o Player 2 (Setas para cima e para baixo)
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                moveInput = 1;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                moveInput = -1;
+            }
+        }
+
+        // Aplicar o movimento ao Rigidbody
+        rb.linearVelocity = new Vector2(0, moveInput * moveSpeed);
     }
 }
